2d5ad33c8d0ea21b2fd332ebc8cdacf469804eb2
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-10-31 17:29:53 +0100

    Create package

---
+++ src/Queries/AlterTableQuery.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Sentience\Database\Queries;
+
+use Sentience\Database\DatabaseInterface;
+use Sentience\Database\Dialects\DialectInterface;
+use Sentience\Database\Queries\Objects\QueryWithParams;
+use Sentience\Database\Queries\Objects\Raw;
+use Sentience\Database\Queries\Traits\AltersTrait;
+use Sentience\Database\Results\ResultInterface;
+
+class AlterTableQuery extends Query
+{
+    use AltersTrait;
+
+    public function __construct(DatabaseInterface $database, DialectInterface $dialect, string|array|Raw $table)
+    {
+        parent::__construct($database, $dialect, $table);
+    }
+
+    public function toQueryWithParams(): array
+    {
+        return $this->dialect->alterTable(
+            $this->table,
+            $this->alters
+        );
+    }
+
+    public function toSql(): array
+    {
+        $queriesWithParams = $this->toQueryWithParams();
+
+        return array_map(
+            fn (QueryWithParams $queryWithParams): string => $queryWithParams->toSql($this->dialect),
+            $queriesWithParams
+        );
+    }
+
+    public function execute(bool $emulatePrepares = false): array
+    {
+        $queriesWithParams = $this->toQueryWithParams();
+
+        return array_map(
+            fn (QueryWithParams $queryWithParams): ResultInterface => $this->database->queryWithParams(
+                $queryWithParams,
+                $emulatePrepares
+            ),
+            $queriesWithParams
+        );
+    }
+}
---
+++ src/Queries/DeleteQuery.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sentience\Database\Queries;
+
+use Sentience\Database\DatabaseInterface;
+use Sentience\Database\Dialects\DialectInterface;
+use Sentience\Database\Queries\Objects\QueryWithParams;
+use Sentience\Database\Queries\Objects\Raw;
+use Sentience\Database\Queries\Traits\ReturningTrait;
+use Sentience\Database\Queries\Traits\WhereTrait;
+use Sentience\Database\Results\ResultInterface;
+
+class DeleteQuery extends Query
+{
+    use ReturningTrait;
+    use WhereTrait;
+
+    public function __construct(DatabaseInterface $database, DialectInterface $dialect, string|array|Raw $table)
+    {
+        parent::__construct($database, $dialect, $table);
+    }
+
+    public function toQueryWithParams(): QueryWithParams
+    {
+        return $this->dialect->delete(
+            $this->table,
+            $this->where,
+            $this->returning
+        );
+    }
+
+    public function toSql(): string
+    {
+        return parent::toSql();
+    }
+
+    public function execute(bool $emulatePrepare = false): ResultInterface
+    {
+        return parent::execute($emulatePrepare);
+    }
+}
