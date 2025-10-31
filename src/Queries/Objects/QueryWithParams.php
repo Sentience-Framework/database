ecf4885ed8aeb02361a4ed22cd508f5d89d05018
UniForceMusic <51342446+UniForceMusic@users.noreply.github.com>
2025-10-31 17:29:53 +0100

    Create package

---
+++ src/Queries/Objects/QueryWithParams.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Sentience\Database\Queries\Objects;
+
+use Throwable;
+use Sentience\Database\Dialects\DialectInterface;
+use Sentience\Database\Exceptions\QueryWithParamsException;
+
+class QueryWithParams
+{
+    public const string INI_PCRE_JIT = 'pcre.jit';
+    public const string INI_PCRE_BACKTRACE_LIMIT = 'pcre.backtrack_limit';
+    public const string INI_PCRE_RECURSION_LIMIT = 'pcre.recursion_limit';
+    public const string REGEX_PATTERN_QUESTION_MARKS = '/(?:\'(?:\\\\.|[^\\\\\'])*\'|\"(?:\\\\.|[^\\\\\"])*\"|\`(?:\\\\.|[^\\\\\`])*\`|(\?)(?=(?:[^\'\"\`\\\\]|\'(?:\\\\.|[^\\\\\'])*\'|\"(?:\\\\.|[^\\\\\"])*\"|\`(?:\\\\.|[^\\\\\`])*\`)*$)|(?:\-\-[^\r\n]*|\/\*[\s\S]*?\*\/|\#.*))/m';
+    public const string REGEX_PATTERN_NAMED_PARAMS = '/(?:\'(?:\\\\.|[^\\\\\'])*\'|\"(?:\\\\.|[^\\\\\"])*\"|\`(?:\\\\.|[^\\\\\`])*\`|(?<!\:)(\:\w+)(?=(?:[^\'\"\`\\\\]|\'(?:\\\\.|[^\\\\\'])*\'|\"(?:\\\\.|[^\\\\\"])*\"|\`(?:\\\\.|[^\\\\\`])*\`)*$)|(?:\-\-[^\r\n]*|\/\*[\s\S]*?\*\/|\#.*))/m';
+
+    public function __construct(
+        public string $query,
+        public array $params = []
+    ) {
+    }
+
+    public function namedParamsToQuestionMarks(): static
+    {
+        if (count($this->params) == 0) {
+            return $this;
+        }
+
+        $params = [];
+
+        $query = $this->pregReplaceCallback(
+            static::REGEX_PATTERN_NAMED_PARAMS,
+            function (array $match) use (&$params): string {
+                if (!$this->isQuestionMarkOrNamedParamMatch($match)) {
+                    return $match[0];
+                }
+
+                $key = $match[1];
+
+                if (!array_key_exists($key, $this->params)) {
+                    $this->throwNamedParamDoesNotExistException($key);
+                }
+
+                $params[] = $this->params[$key];
+
+                return '?';
+            },
+            $this->query
+        );
+
+        if (count($params) == 0) {
+            return $this;
+        }
+
+        $this->query = $query;
+        $this->params = $params;
+
+        return $this;
+    }
+
+    public function toSql(DialectInterface $dialect): string
+    {
+        if (count($this->params) == 0) {
+            return $this->query;
+        }
+
+        $params = array_map(
+            fn (mixed $param): mixed => $dialect->castToQuery($param),
+            $this->params
+        );
+
+        return array_is_list($params)
+            ? $this->toSqlQuestionMarks($params)
+            : $this->toSqlNamedParams($params);
+    }
+
+    protected function toSqlQuestionMarks(array $params): string
+    {
+        $index = 0;
+
+        return $this->pregReplaceCallback(
+            static::REGEX_PATTERN_QUESTION_MARKS,
+            function (array $match) use ($params, &$index): string {
+                if (!$this->isQuestionMarkOrNamedParamMatch($match)) {
+                    return $match[0];
+                }
+
+                if (!array_key_exists($index, $params)) {
+                    throw new QueryWithParamsException('question mark and value count do not match');
+                }
+
+                $value = $params[$index];
+
+                $index++;
+
+                return (string) $value;
+            },
+            $this->query
+        );
+    }
+
+    protected function toSqlNamedParams(array $params): string
+    {
+        return $this->pregReplaceCallback(
+            static::REGEX_PATTERN_NAMED_PARAMS,
+            function (array $match) use ($params): string {
+                if (!$this->isQuestionMarkOrNamedParamMatch($match)) {
+                    return $match[0];
+                }
+
+                $key = $match[1];
+
+                if (!array_key_exists($key, $params)) {
+                    $this->throwNamedParamDoesNotExistException($key);
+                }
+
+                return (string) $params[$key];
+            },
+            $this->query
+        );
+    }
+
+    protected function pregReplaceCallback(string|array $pattern, callable $callback, string|array $subject): null|string|array
+    {
+        $iniPcreJit = ini_get(static::INI_PCRE_JIT);
+        $iniPcreBacktraceLimit = ini_get(static::INI_PCRE_BACKTRACE_LIMIT);
+        $iniPcreRecursionLimit = ini_get(static::INI_PCRE_RECURSION_LIMIT);
+
+        ini_set(static::INI_PCRE_JIT, (string) 0);
+        ini_set(static::INI_PCRE_BACKTRACE_LIMIT, (string) PHP_INT_MAX);
+        ini_set(static::INI_PCRE_RECURSION_LIMIT, (string) PHP_INT_MAX);
+
+        try {
+            return preg_replace_callback($pattern, $callback, $subject);
+        } catch (Throwable $exception) {
+            throw $exception;
+        } finally {
+            if (!is_bool($iniPcreJit)) {
+                ini_set(static::INI_PCRE_JIT, $iniPcreJit);
+            }
+
+            if (!is_bool($iniPcreBacktraceLimit)) {
+                ini_set(static::INI_PCRE_BACKTRACE_LIMIT, $iniPcreBacktraceLimit);
+            }
+
+            if (!is_bool($iniPcreRecursionLimit)) {
+                ini_set(static::INI_PCRE_RECURSION_LIMIT, $iniPcreRecursionLimit);
+            }
+        }
+    }
+
+    protected function isQuestionMarkOrNamedParamMatch(array $match): bool
+    {
+        return count($match) > 1;
+    }
+
+    protected function throwNamedParamDoesNotExistException(string $key): void
+    {
+        throw new QueryWithParamsException("named param {$key} does not exist");
+    }
+}
---
+++ src/Queries/Traits/JoinsTrait.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Sentience\Database\Queries\Traits;
+
+use DateTimeInterface;
+use Sentience\Database\Queries\Enums\JoinEnum;
+use Sentience\Database\Queries\Interfaces\Sql;
+use Sentience\Database\Queries\Objects\Alias;
+use Sentience\Database\Queries\Objects\Join;
+use Sentience\Database\Queries\Objects\SubQuery;
+use Sentience\Database\Queries\Query;
+use Sentience\Database\Queries\SelectQuery;
+
+trait JoinsTrait
+{
+    protected array $joins = [];
+
+    public function leftJoin(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::LEFT_JOIN, $table, $on);
+    }
+
+    public function leftJoinTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->leftJoin($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function leftJoinSubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->leftJoin(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function leftJoinLateral(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::LEFT_JOIN_LATERAL, $table, $on);
+    }
+
+    public function leftJoinLateralTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->leftJoinLateral($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function leftJoinLateralSubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->leftJoinLateral(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function innerJoin(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::INNER_JOIN, $table, $on);
+    }
+
+    public function innerJoinTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->innerJoin($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function innerJoinSubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->innerJoin(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function innerJoinLateral(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::INNER_JOIN_LATERAL, $table, $on);
+    }
+
+    public function innerJoinLateralTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->innerJoinLateral($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function innerJoinLateralSubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->innerJoinLateral(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function outerApply(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::LEFT_JOIN_LATERAL, $table, $on);
+    }
+
+    public function outerApplyTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->outerApply($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function outerApplySubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->outerApply(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function crossApply(string|array|Alias|Sql|SubQuery $table, ?callable $on = null): static
+    {
+        return $this->addJoin(JoinEnum::INNER_JOIN_LATERAL, $table, $on);
+    }
+
+    public function crossApplyTable(string|array|Sql $table, ?callable $on = null, ?string $alias = null): static
+    {
+        return $this->crossApply($alias ? Query::alias($table, $alias) : $table, $on);
+    }
+
+    public function crossApplySubQuery(SelectQuery $selectQuery, string $alias, ?callable $on = null): static
+    {
+        return $this->crossApply(Query::subQuery($selectQuery, $alias), $on);
+    }
+
+    public function joinf(string $format, null|bool|int|float|string|DateTimeInterface|SelectQuery|Sql ...$values): static
+    {
+        $this->joins[] = Query::expressionf($format, ...$values);
+
+        return $this;
+    }
+
+    public function join(string $join): static
+    {
+        $this->joins[] = Query::raw($join);
+
+        return $this;
+    }
+
+    protected function addJoin(JoinEnum $join, string|array|Alias|Sql|SubQuery $table, ?callable $on): static
+    {
+        $join = new Join($join, $table);
+
+        if ($on) {
+            $join = $on($join) ?? $join;
+        }
+
+        if (!($join instanceof Join)) {
+            return $this;
+        }
+
+        $this->joins[] = $join;
+
+        return $this;
+    }
+}
